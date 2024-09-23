5f4602bf6f5fffdb7df6fbed81a377e781ace18b
Hvemvilhvad <malthewbirch@gmail.com>
2024-09-23 15:37:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dania_Basis_programering_aflevering_Variabler___Datatyper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
